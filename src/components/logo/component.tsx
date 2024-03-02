74644c7b2cbace7bd1249ef534b009c03e4b0968
Adrian Urs <adrian.urs00@gmail.com>
2024-03-02 17:45:15 +0200

    added husky

--- src/components/logo/component.tsx
+++ src/components/logo/component.tsx
@@ -2,8 +2,6 @@ import { LogoSrc } from '@/assets';
 import { LogoFC } from './types';
 import Image from 'next/image';
 
-export const Logo: LogoFC = ({width, height}) => {
-	return (
-		<Image src={LogoSrc} alt="agency logo" width={width || 70} height={height || 70} priority />
-	)
-}
+export const Logo: LogoFC = ({ width, height }) => {
+	return <Image src={LogoSrc} alt='Next agency logo' width={width || 70} height={height || 70} priority />;
+};
